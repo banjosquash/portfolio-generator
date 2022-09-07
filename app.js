5f5de83f527bb5e210b780e18c1a414fb3805f38
Brayden <banjosquash@gmail.com>
2022-09-07 14:14:49 -0400

    built an html generator in node

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
 
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArrs => {
-    for (let i = 0; i < profileDataArrs.length; i++){
-        console.log(profileDataArrs[i]);
-    }
-    
-};
 
-printProfileData(profileDataArgs)
+
+fs.writeFile('index.html', generatePage(name, github), err => {
+    if (err) throw new Error (err);
+  
+    console.log('Portfolio complete! Check out index.html to see the output!');
+  });
